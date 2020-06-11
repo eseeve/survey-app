4411b8391e2981f7385c314c3451ec8977d58eb8
Elias Seeve <elias.seeve@gmail.com>
2020-06-11 16:47:47 +0300

    Fixing heroku url path

--- survey-frontend/src/services/surveys.js
+++ survey-frontend/src/services/surveys.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/surveys'
+const baseUrl = '/api/surveys'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
