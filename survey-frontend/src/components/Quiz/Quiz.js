68787477716a1fa591fa0b3ae5a648e349a12a6f
Elias Seeve <elias.seeve@gmail.com>
2020-07-16 13:19:08 +0300

    Added score after completing a quiz

--- survey-frontend/src/components/Quiz/Quiz.js
+++ survey-frontend/src/components/Quiz/Quiz.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams, useHistory } from 'react-router-dom'
-import { Header, Grid } from 'semantic-ui-react'
+import { Header, Grid, Button } from 'semantic-ui-react'
 import { SubmissionError } from 'redux-form'
 
 import { setNotification } from '../../reducers/notificationReducer'
 import TakeQuiz from './TakeQuiz'
 import Menu from '../Menu'
 import { answerQuiz } from '../../reducers/quizReducer'
+import Notification from '../Notification'
 
 
-const Survey = () => {
+const Quiz = () => {
+  const [ score, setScore ] = useState(null)
   const quizzes = useSelector(state => state.quizzes)
   const dispatch = useDispatch()
   const history = useHistory()
@@ -24,14 +26,45 @@ const Survey = () => {
         _error: 'You must answer to all questions'
       })
     } else {
+      setScore(0)
+      quiz.questions.forEach(q => {
+        if (q.options[q.correct].option === values[q.title]) {
+          setScore(score + 1)
+        }
+      })
       dispatch(answerQuiz(quiz, values))
-      dispatch(setNotification(`Your answers to the survey '${quiz.name}' were saved!`, 5))
-      history.push('/quizzes')
+      dispatch(setNotification(`Your answers to the quiz '${quiz.name}' were saved!`, 5))
     }
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    setScore(null)
+    history.push('/quizzes')
   }
 
   if (!quiz) {
     return null
+  }
+
+  if (score !== null) {
+    return (
+      <div>
+      <Grid style={{paddingTop: '10px', marginBottom: '10px'}} columns='equal'>
+        <Grid.Column width={14}>
+          <Header as='h1' >Your score on {quiz.name}</Header>
+        </Grid.Column>
+        <Grid.Column style={{marginTop: '5px'}} >
+          <Menu link='Home' />
+        </Grid.Column>
+      </Grid>
+      <Notification />
+      <div>
+        You got {score}/{quiz.questions.length} answers correct
+      </div>
+      <Button className='teal-button' color='teal' style={{marginTop: '10px', marginBottom: '10px'}} onClick={handleClick} floated='left'>Back to quizzes</Button>
+    </div>
+    )
   }
 
   return (
@@ -49,4 +82,4 @@ const Survey = () => {
   )
 }
 
-export default Survey
+export default Quiz
